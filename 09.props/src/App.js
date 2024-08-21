9e62333407f3dbe2be07e8e8696ae2aac147b6b6
0biin0 <infinite402@gmail.com>
2024-08-21 14:40:32 +0900

    0821

--- 09.props/src/App.js
+++ 09.props/src/App.js
@@ -4,8 +4,9 @@ import Header from './component/Header';
 import Body from './component/Body';
 import Footer from './component/Footer';
 
+/*
+// 1. 변수에 담긴값을 Body로 넘겨주기
 function App() {
-  // 변수에 담긴값을 Body로 넘겨주기
   const addr = "서초구 강남대로";
   const name = "이고잉";
 
@@ -14,6 +15,27 @@ function App() {
         <Header />
         <section>
            <Body address = {addr} user={name}/> 
+        </section>
+        <Footer />
+    </div>
+  );
+}
+*/
+
+//2.객체 형태로 넘기기
+function App() {
+  const userInfo = {
+    name : "이고잉",
+    addr : "서초구 강남대로",
+    likeList : ["캐릭터", "만화", "웹툰"]
+  }
+  return (
+    <div className="App">
+        <Header />
+        <section>
+           {/* <Body userInfo={userInfo}/>  // 2.1 */}
+           {/*<Body {...userInfo} /> */}{/*  // 2.2 */}
+           <Body /> {/* //3 */}
         </section>
         <Footer />
     </div>
--- 09.props/src/component/Body.js
+++ 09.props/src/component/Body.js
@@ -1,3 +1,7 @@
+import Button from "./Button";
+/*
+import Body from './Body';
+//1. 부모로부터 변수의 값 넘겨 받기
 const Body = (props) => {
     return (
         <>
@@ -7,4 +11,49 @@ const Body = (props) => {
         </>
     )
 }
+*/
+
+/*
+//2.1 객체형태로 넘겨준 값 받기
+const Body = (props) => {
+    return (
+        <>
+            <h3>본문입니다</h3>
+            <p>{props.userInfo.name}님은 {props.userInfo.addr}에 살고 있습니다</p>
+            <p>내가 좋아하는 것들은 {props.userInfo.likeList}의 {props.userInfo.likeList.length}개를 좋아합니다</p>
+            <p>그 중에서 가장 좋아하는 것은 {props.userInfo.likeList[2]}입니다</p>
+        </>
+    )
+}
+*/
+/*
+//2.2 객체형태의 값을 풀어서 넘겨준 값 받기
+const Body = ({name, addr, likeList}) => {
+    return (
+        <>
+            <h3>본문입니다</h3>
+            <p>{name}님은 {addr}에 살고 있습니다</p>
+            <p>내가 좋아하는 것들은 {likeList}의 {likeList.length}개를 좋아합니다</p>
+            <p>그 중에서 가장 좋아하는 것은 {likeList[2]}입니다</p>
+        </>
+    )
+}
+*/
+
+const Body = () => {
+    function btnClick(e) {
+        alert("버튼 눌렀어요");
+        console.log(e);
+    }
+    return (
+        <>
+            <h3>본문입니다</h3>
+            <button onClick={btnClick}>A이벤트 버튼</button><br/><br/>
+            <button onClick={btnClick}>B이벤트 버튼</button><br/><br/>
+            <Button />
+           
+        </>
+    )
+}
+
 export default Body;
