e477de458eab57cbc78e7283546186c1cc231a9b
0biin0 <infinite402@gmail.com>
2024-08-23 16:13:09 +0900

    0823

--- 12.shopping-mall/src/pages/Detail.js
+++ 12.shopping-mall/src/pages/Detail.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import {Container,Row, Button, Col } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import styled from 'styled-components';
 
 /*
-    lifeCycle(생명주기)
-    컴포넌트
-    1. 생성
+   - useEffect(() => {실행할 코드}, [변경되는 state])
+     변경되는 state가 변경되어 재렌더링 될 때만 호출이 됨
+     이외의 재렌더링시에는 호출되지 않음
+    - useEffect(() => {실행할 코드},[])
+      최초 mount될 때 한번만 사용
+
+    - useEffect(() => { 변수.. return(리턴할 코드)}, [])
+      return문법 : clear up function이라 한다
+      useEffect가 실행되기 전에 return을 먼저 실행
+      return은 mount시 실행 안되고, unmount시에만 실행됨
+
+      * 정리
+      useEffect(() => {}) : 재렌더링 될떄마다 실행
+      useEffect(() => {},{}) : mount시 한번만 실행
+      useEffect(() => {}, [???]) : ???이 재렌더링 될때마다 실행
+      useEffect(() => {.. return( unmount시 한번 실행)}])
 */
 
 function Detail (props) {
@@ -14,9 +28,35 @@ function Detail (props) {
       let findId = props.clothes.find(function(x) {
         return x.id == index;
       })
+    
+    //   let [alert, setAlert] = useState(true);
+    //   let [count, setCount] = useState(0);
+
+    //   useEffect(() => {
+    //    let timer = setTimeout(() => {setAlert(false)}, 1000)
+    //     return () =>{
+    //         // 기존 타이머 삭제
+    //         clearTimeout(timer);
+    //     }
+        
+    //   },[alert])
+    // alert창 띄우기
+
+    let[num, setNum] = useState('');
+      useEffect(() => {
+        if(isNaN(num) == true) { // Not a Number 숫자이면 false, 문자이면 true
+            alert('그러지마요');
+        }
+      },[num])
 
     return (
         <div>
+            <input onChange={(e) => {setNum(e.target.value)}} />
+            {/* {alert ? <h2>2초 이내 구매시 할인</h2> : null}
+            <button onClick={() => {setAlert(true)}}>alert 버튼</button>
+
+            {count}
+            <button onClick={() => {setCount(count+1)}}>count 버튼</button> */}
             <Container>
             <Row>
                 <Col lg={6}>
